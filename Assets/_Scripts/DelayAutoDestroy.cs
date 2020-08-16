7d1f0ab293b18640f71e87e3592da87e294c9976
Swordman999 <powerapp2001@gmail.com>
2020-08-16 14:11:48 -0700

    Create DelayAutoDestroy.cs

---
+++ Assets/_Scripts/DelayAutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAutoDestroy : MonoBehaviour {
+	public float lifeDuration = 2.0f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (waitAndDestory());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator waitAndDestory() {
+		yield return new WaitForSeconds (lifeDuration);
+		Destroy (gameObject);
+	}
+}
