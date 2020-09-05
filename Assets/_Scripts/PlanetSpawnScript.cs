243cd886484858ce7ff121704ccd736570c6cbaa
busyboy5483 <zhonglucas@gmail.com>
2020-09-04 20:28:19 -0700

    stopped planet spawning issue

--- Assets/_Scripts/PlanetSpawnScript.cs
+++ Assets/_Scripts/PlanetSpawnScript.cs
@@ -12,7 +12,7 @@ public class PlanetSpawnScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnPlanet());
+        //StartCoroutine(spawnPlanet());
     }
 
     // Update is called once per frame
